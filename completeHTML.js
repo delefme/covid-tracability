824ec3387d43e1b994c83797c8bdd984d39c643c
Dani Vilardell <danivilardell@gmail.com>
2020-09-23 16:00:40 +0200

    Programa basic acabat, funciona

--- completeHTML.js
+++ completeHTML.js
@@ -3,10 +3,8 @@ function completaHTML() {
     document.getElementById("data").innerHTML = data;
     var trobat = false;
     for (i = 0; i < dataHorarisAlumne.length; i++) { 
-    	var h = dataHorarisAlumne[i].hora.split(":");
+    	var h = dataHorarisAlumne[i].hora[hora_actual.getDay()].split(":");
 		var hAct = hora_actual.getHours() - 1;
-		console.log(h[0]);
-		console.log(hAct);
         if(hAct < 10) hAct = "0" + hAct;
         if(h[0] == hAct) {
         	trobat = true;
--- completeSubjectListInHTML.js
+++ completeSubjectListInHTML.js
@@ -1,19 +1,36 @@
 
 function completeSubjectList() {
 	var trobat = false;
+	var jaEsta = false;
 	for(var i = 0; i < totesLesClasses.length; i++) {
+
 		if(document.getElementById("myInput").value == totesLesClasses[i].nom) {
-			dataHorarisAlumne.push({ "nom":totesLesClasses[i].nom, "hora":totesLesClasses[i].hora, "aula":totesLesClasses[i].aula })
-			document.getElementById("subjectList").innerHTML += "<br>" +  document.getElementById("myInput").value  + " " + dataHorarisAlumne[i].hora;
-	    	localStorage.dataHorarisAlumne = JSON.stringify(dataHorarisAlumne);
-			trobat = true;
-			completaHTML();
+			for(var j = 0; j < dataHorarisAlumne.length; j++) {
+				if(totesLesClasses[i].nom == dataHorarisAlumne[j].nom) {
+					jaEsta = true;
+					break;
+				}
+			}
+			if(!jaEsta) {
+				dataHorarisAlumne.push({ "nom":totesLesClasses[i].nom, "hora":totesLesClasses[i].hora, "aula": totesLesClasses[i].aula })
+				document.getElementById("subjectList").innerHTML += "<br>" +  document.getElementById("myInput").value  + " " + totesLesClasses[i].hora[hora_actual.getDay()];
+		    	localStorage.dataHorarisAlumne = JSON.stringify(dataHorarisAlumne);
+				trobat = true;
+				completaHTML();
+			}
+			else {
+				break;
+			}
 		}
 	}
-	if(!trobat) {
+	if(!trobat && !jaEsta) {
 		showAlert();
 	}
+	else if(jaEsta) {
+
+	}
 }
+
 
 function returnAllSubjectArray() {
 	for(var i = 0; i < totesLesClasses.length; i++) {
@@ -23,8 +40,7 @@ function returnAllSubjectArray() {
 
 function writeSubjectList() {
 	for(var i = 0; i < dataHorarisAlumne.length; i++) {
-		console.log("hola");
-        document.getElementById("subjectList").innerHTML += "<br>" +  dataHorarisAlumne[i].nom + " " + dataHorarisAlumne[i].hora;
+        document.getElementById("subjectList").innerHTML += "<br>" +  dataHorarisAlumne[i].nom + " " + dataHorarisAlumne[i].hora[hora_actual.getDay()];
     } 
 }
 
